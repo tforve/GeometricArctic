27dc2137bdcedf344454950eea41e16473915c38
Forve <tobias.forve@gmx.de>
2018-10-25 22:48:41 +0200

    Up

---
+++ GeometricArctic/Assets/Scripts/Controller/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public CharacterController2D controller;
+	
+	public float runSpeed = 40.0f;
+		
+	private float horizontalMove = 0.0f;
+
+	private bool jump = false;
+	private bool crouch = false;
+	
+	
+
+	// Update is called once per frame
+	void Update ()
+	{
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+		
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+		
+	}
+}
