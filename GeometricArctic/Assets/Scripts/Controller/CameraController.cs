c420e75768c71acc32a62793c7895452c52b92ee
Unknown <tobias.forve@gmx.de>
2018-10-27 01:27:56 +0200

    up

---
+++ GeometricArctic/Assets/Scripts/Controller/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+	[SerializeField] private GameObject target;
+
+	void Update()
+	{
+		if (target)
+		{
+			transform.position = Vector3.Lerp(transform.position, target.transform.position, 0.1f) + new Vector3(0, 1.0f ,-3.8f);
+		}
+	}
+
+}
