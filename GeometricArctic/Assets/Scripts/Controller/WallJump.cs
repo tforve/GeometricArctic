b5a2103bfc8a008bce18788decee12f57be7641a
Unknown <tobias.forve@gmx.de>
2019-04-04 22:50:49 +0200

    Added Simple Walljumps
    - need to change that can only jump in other direction

---
+++ GeometricArctic/Assets/Scripts/Controller/WallJump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallJump : MonoBehaviour
+{
+    [SerializeField] private float maxDistance = 2.85f;
+    private CharacterController3D characterController;
+    public LayerMask m_WhatIsWall;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController3D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        
+        if(Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.right), out hit, maxDistance, m_WhatIsWall) ||Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.left), out hit, maxDistance, m_WhatIsWall) )
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * maxDistance, Color.red);
+            characterController.MyIsGrounded = true;            
+        }        
+    }
+}
