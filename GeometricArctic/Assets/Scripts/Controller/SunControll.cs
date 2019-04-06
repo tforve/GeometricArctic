996a0fe6249bf541829f47795869b3d572808a31
Forve <tobias.forve@gmx.de>
2019-04-06 17:35:38 +0200

    Added MainMenu
    -  MainMenu Scene added

---
+++ GeometricArctic/Assets/Scripts/Controller/SunControll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunControll : MonoBehaviour
+{
+    	//Range for min/max values of variable
+	[Range(-10f, 10f)]
+	public float XRotationSpeed, YRotationSpeed;
+
+	// Sun Movement
+	void Update () {
+		gameObject.transform.Rotate (XRotationSpeed * Time.deltaTime, YRotationSpeed * Time.deltaTime, 0);
+	}
+}
