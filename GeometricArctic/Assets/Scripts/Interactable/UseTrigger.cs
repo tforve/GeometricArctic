9bf69328078cd241b7d0b29f8599aa21689812a6
burly87 <tobias.forve@gmx.de>
2019-04-07 09:41:39 +0200

    Change Station working on UseTrigger

--- GeometricArctic/Assets/Scripts/Interactable/UseTrigger.cs
+++ GeometricArctic/Assets/Scripts/Interactable/UseTrigger.cs
@@ -7,15 +7,14 @@ public class UseTrigger : MonoBehaviour
 {
     private Light halo;
     private bool isUsed = false;
-    private float timeToReachTarget;
+    [SerializeField] private float timeToReachTarget = 5.0f;
     private Vector3 target, startPos;
-    float lerpTime;
+    private float lerpTime;
 
     void Start()
     {
         halo = GetComponent<Light>();
         startPos = this.transform.position;
-        target = startPos.transform.Translate(0.0f, -0.25f, 0.0f);
     }
 
     private void FlickeringHalo()
@@ -26,19 +25,21 @@ public class UseTrigger : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !isUsed)
-        {
+        {   
+            target = startPos;
+            target += new Vector3(-10.0f, -0.0f, 0.0f);
             lerpTime += Time.deltaTime / timeToReachTarget;
             this.transform.position = Vector3.Lerp(startPos, target, lerpTime);//Translate(0.0f, -0.25f, 0.0f);
             isUsed = true;
         }
     }
 
-    public void SetDestination(Vector3 destination, float time)
-    {
-        lerpTime = 0;
-        startPos = transform.position;
-        timeToReachTarget = time;
-        target = destination;
-    }
+    // public void SetDestination(Vector3 destination, float time)
+    // {
+    //     lerpTime = 0;
+    //     startPos = transform.position;
+    //     timeToReachTarget = time;
+    //     target = destination;
+    // }
 
 }
